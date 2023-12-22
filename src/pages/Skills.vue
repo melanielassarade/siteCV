b2fbfc7d781360c97bd99b34b810f5b8900d1eee
melanielassarade <melanie.lassarade@gmail.com>
2023-12-22 17:01:47 +0100

    expérimentation morph-penser a modif les script sur autres pages

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -4,16 +4,86 @@
         <q-page-section id="headerSkillsPage">
             <h3 class="text-uppercase text-center">Compétences</h3>
         </q-page-section>
+        <q-page-section>
+            <div class="q-pa-md relative-position" style="height: 60vh">
+                <q-btn v-morph:btn:mygroup:300.resize="morphGroupModel" class="absolute-bottom-left q-ma-md" fab color="primary" size="lg" icon="add" label="Compétences Techniques" @click="nextMorph" />
+            
+                <q-card v-morph:card1:mygroup:500.resize="morphGroupModel" class="absolute-bottom-left q-ma-md bg-primary text-white" style="width: 80vw; border-bottom-left-radius: 2em">
+                    <q-card-section class="text-h6">
+                        Techniques numériques
+                    </q-card-section>
+            
+                    <q-card-section class="text-subtitle1">
+                        <q-list bordered separator>
+                            <q-item v-ripple>
+                                <q-item-section>
+                                    <q-item-label overline>Développement</q-item-label>
+                                    <q-item-label>Utilisation et gestion du CMS Wordpress</q-item-label>
+                                </q-item-section>
+                            </q-item>
+                    
+                            <q-item v-ripple>
+                                <q-item-section>
+                                    <q-item-label overline>Développement</q-item-label>
+                                    <q-item-label>Langages et framework orienté back-end : PHP, POO, Symphony, Git</q-item-label>
+                                </q-item-section>
+                            </q-item>
+                    
+                            <q-item v-ripple>
+                                <q-item-section>
+                                    <q-item-label overline>Développement</q-item-label>
+                                    <q-item-label>Langages et framework orienté front-end : HTML5, CSS3, Javascript, Bootstrap, Quasar, Node.JS, Vue.JS</q-item-label>
+                                </q-item-section>
+                            </q-item>
+
+                            <q-item v-ripple>
+                                <q-item-section>
+                                    <q-item-label overline>Développement</q-item-label>
+                                    <q-item-label>Construction et exploitation de base de données avec MySQL</q-item-label>
+                                </q-item-section>
+                            </q-item>
+
+                            <q-item v-ripple>
+                                <q-item-section>
+                                    <q-item-label overline>Design</q-item-label>
+                                    <q-item-label>Maîtrise des logiciels de la suite Adobe CC -Photoshop-Illustrator-InDesign-XD-</q-item-label>
+                                </q-item-section>
+                            </q-item>
+
+                        </q-list>
+                    </q-card-section>
+            
+                    <q-card-actions align="right">
+                        <q-btn flat label="Fermer" @click="nextMorph" />
+                    </q-card-actions>
+                </q-card>            
+            </div>
+        </q-page-section>
     </q-page>
 
 </template>
 
 <script>
 
-    import { defineComponent } from 'vue';
+    import { defineComponent, ref } from 'vue';
+
+    const nextMorphStep = {
+        btn: 'card1',
+        card1: 'btn',
+    }
 
     export default defineComponent({
-        name: "Skills"
-    })
+        name: "Skills",
 
+        setup () {
+            const morphGroupModel = ref('btn')
+        
+            return {
+                morphGroupModel,
+                nextMorph () {
+                    morphGroupModel.value = nextMorphStep[ morphGroupModel.value ]
+                }
+            }
+        }
+    })
 </script>
